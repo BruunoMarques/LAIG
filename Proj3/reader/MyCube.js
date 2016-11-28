bb9446e93d499fdfb22a206a877235ed2f5c8a86
zecst19 <zecst19@gmail.com>
2016-11-28 13:25:45 +0000

    mais outro

--- Proj3/reader/MyCube.js
+++ Proj3/reader/MyCube.js
@@ -3,15 +3,9 @@ function MyCube(scene, x, y, z, dim) {
     CGFobject.call(this, scene);
 
     this.scene = scene;
-    this.topo = topo;
-    this.base = base;
-    this.height = height;
-    this.slices = slices;
-    this.stacks = stacks;
-    this.diference = (this.base - this.topo) / this.stacks;
-    this.circleTop;
-    this.circleBas;
-    this.body;
+    this.x = x;
+    this.y = y;
+    this.dim = dim;
 
     this.initBuffers();
 }
@@ -20,20 +14,13 @@ MyCube.prototype.constructor = MyCube;
 
 /* Inicializa as caracteristicas do body do cilindro */
 MyCube.prototype.initBuffers = function() {
+  this.square = MyRectangle(this.scene,0,this.dim,0,this.dim);
 }
 
 MyCube.prototype.display = function(){
     this.scene.pushMatrix();
     this.scene.translate(0,0,this.height);
-    this.circleTop.display();
+    this.square.display();
     this.scene.popMatrix();
 
-    this.scene.pushMatrix();
-    this.scene.rotate(Math.PI,1,0,0);
-    this.circleBas.display();
-    this.scene.popMatrix();
-
-    this.scene.pushMatrix();
-    this.body.display();
-    this.scene.popMatrix();
 }
