d9fe36f6c847f8e646b7a784dc1f9893bc86e266
zecst19 <zecst19@gmail.com>
2016-11-30 16:30:01 +0000

    peças done, falta por todas e por no sitio

--- Proj3/reader/MyNewPiece.js
+++ Proj3/reader/MyNewPiece.js
@@ -25,11 +25,57 @@ MyNewPiece.prototype.display = function(){
 				this.scene.scale(0.9,1,0.9);
 				this.part.display();
 				this.scene.popMatrix();
+
 				this.scene.pushMatrix();
 				this.scene.scale(1,2,1);
 				this.part.display();
 				this.scene.popMatrix();
+
 				break;
+			case 1:
+				this.scene.pushMatrix();
+				this.scene.translate(0,6,0);
+				this.scene.scale(0.8,1,0.8);
+				this.part.display();
+				this.scene.popMatrix();
+
+				this.scene.pushMatrix();
+				this.scene.translate(0,2.7,0);
+				this.scene.scale(0.9,1,0.9);
+				this.part.display();
+				this.scene.popMatrix();
+
+				this.scene.pushMatrix();
+				this.scene.scale(1,2,1);
+				this.part.display();
+				this.scene.popMatrix();
+
+				break;
+			case 2:
+				this.scene.pushMatrix();
+				this.scene.translate(0,9,0);
+				this.scene.scale(0.6,1,0.6);
+				this.part.display();
+				this.scene.popMatrix();
+
+				this.scene.pushMatrix();
+				this.scene.translate(0,6,0);
+				this.scene.scale(0.8,1,0.8);
+				this.part.display();
+				this.scene.popMatrix();
+
+				this.scene.pushMatrix();
+				this.scene.translate(0,3,0);
+				this.scene.scale(0.9,1,0.9);
+				this.part.display();
+				this.scene.popMatrix();
+
+				this.scene.pushMatrix();
+				this.scene.scale(1,2,1);
+				this.part.display();
+				this.scene.popMatrix();
+
+					break;
 			default:
 
 		}
